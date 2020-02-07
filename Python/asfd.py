1c18f81bbb8467028692dd3e142008235b569c85
jmcummings <jmcummings@morsecorp.com>
2020-02-07 16:09:58 -0500

    fixing being an idiot and not adding a .gitignore

--- Python/asfd.py
+++
@@ -1,3 +0,0 @@
-m,s=99,[]
-while~m:j=f"{(m or'no more',99)[m<0]} bottle"+"s of beer on the wall"[m==1:];s+=j.capitalize(),j[:-12];m-=1;s+='GToa kteo  otnhee  dsotwonr ea nadn dp absusy  isto maer omuonrde'[m>-1::2],j
-print('as')
